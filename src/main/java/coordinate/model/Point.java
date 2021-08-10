abe1e94d528a98773b068cab0334a3db8c6a32bc
jiyun-Lee-dev <jiyun.dev0@gmail.com>
2021-08-10 17:31:16 +0900

    feat: point 객체 생성 구현

---
+++ src/main/java/coordinate/model/Point.java
@@ -0,0 +1,41 @@
+package coordinate.model;
+
+import java.util.Objects;
+
+public class Point {
+    private static final int MIN = 1;
+    private static final int MAX = 24;
+    private static final String OUT_OF_RANGE_ERROR_MESSAGE
+            = "잘못된 입력값입니다. 좌표의 범위는 " + Point.MIN + "부터 " + Point.MAX + "까지만 가능합니다.";
+    private final int x;
+    private final int y;
+
+    public Point(int x, int y) {
+        if (checkOfRange(x, y)) {
+            throw new IllegalArgumentException(OUT_OF_RANGE_ERROR_MESSAGE);
+        }
+        this.x = x;
+        this.y = y;
+    }
+
+    private boolean checkOfRange(int x, int y) {
+        return outOfRange(x) || outOfRange(y);
+    }
+
+    private boolean outOfRange(int value) {
+        return value < MIN || value > MAX;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
---
+++ src/test/java/coordinate/model/PointTest.java
@@ -0,0 +1,23 @@
+package coordinate.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class PointTest {
+    @Test
+    void 생성되는지_확인() {
+        assertThat(new Point(1, 2)).isEqualTo(new Point(1, 2));
+    }
+
+    @Test
+    void 잘못된_값_입력하면_에러발생하는지_확인() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Point(0, 1);
+        });
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Point(25, 1);
+        });
+    }
+}
